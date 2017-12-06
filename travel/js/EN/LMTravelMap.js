4a4b09748ca64c62f47066f000247196f7c3ca1d
liaozq009 <245822113@qq.com>
2017-12-06 17:56:06 +0800

    地图和运价规则

--- travel/js/EN/LMTravelMap.js
+++ travel/js/EN/LMTravelMap.js
@@ -7,6 +7,17 @@ var LMTravelList = {
 	/* 其他事件 */
 	otherEvent:function(){
 
+		function MM_reloadPage(init) {
+		    if (init == true) with (navigator) {
+		        if ((appName == "Netscape") && (parseInt(appVersion) == 4)) {
+		            document.MM_pgW = innerWidth; document.MM_pgH = innerHeight; onresize = MM_reloadPage;
+		        }
+		    }else if (innerWidth != document.MM_pgW || innerHeight != document.MM_pgH){
+		        location.reload();
+		    }
+		}
+		MM_reloadPage(true);
+
 		// 获取容器的宽
 		var $con = $('.lm-container');
 		var getHeight = function(){
