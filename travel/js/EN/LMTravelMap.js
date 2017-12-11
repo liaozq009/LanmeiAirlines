847577eaf403b377a5adaa894657e52e4a25047d
liaozq009 <245822113@qq.com>
2017-12-11 17:55:03 +0800

    地图适配

--- travel/js/EN/LMTravelMap.js
+++ travel/js/EN/LMTravelMap.js
@@ -15,25 +15,31 @@ var LMTravelList = {
 			$(this).parent().slideDown('slow').siblings('span').html(val);
 		});
 
-		$('.modal__content').click(function(){
+		$('.modal-content').click(function(){
 			$('.modal-hot ul').slideUp('slow');
 		});
 	},
 
 	/* 其他事件 */
 	otherEvent:function(){
+		// 弹出模态框
+		$('.img-com').click(function(){
+			var local = $(this).attr('data-local');
+			$('#'+local).modal();
+		});
 
-		function MM_reloadPage(init) {
-		    if (init == true) with (navigator) {
-		        if ((appName == "Netscape") && (parseInt(appVersion) == 4)) {
-		            document.MM_pgW = innerWidth; document.MM_pgH = innerHeight; onresize = MM_reloadPage;
-		        }
-		    }else if (innerWidth != document.MM_pgW || innerHeight != document.MM_pgH){
-		        location.reload();
-		    }
-		}
-		MM_reloadPage(true);
+		// function MM_reloadPage(init) {
+		//     if (init == true) with (navigator) {
+		//         if ((appName == "Netscape") && (parseInt(appVersion) == 4)) {
+		//             document.MM_pgW = innerWidth; document.MM_pgH = innerHeight; onresize = MM_reloadPage;
+		//         }
+		//     }else if (innerWidth != document.MM_pgW || innerHeight != document.MM_pgH){
+		//         location.reload();
+		//     }
+		// }
+		// MM_reloadPage(true);
 
+		// 
 		$('.img-com').mousedown(function(event) {
 			$(this).css('transform','scale(0.8)');
 		}).mouseup(function(event) {
